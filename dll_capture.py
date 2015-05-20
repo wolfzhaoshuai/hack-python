ae330e349a77a78f360669caba55355a63978ab0
wolfzhaoshuai <15165268607@163.com>
2015-05-20 10:09:41 +0800

    capture packets on date link layer

---
+++ dll_capture.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import os
+import socket
+import sys
+
+'''
+    Ehternet layer capture
+'''
+
+if len(sys.argv)!=3:
+    print "Usage:./dll_capture Host [IP|ARP]"
+    sys.exit(1)
+
+host=sys.argv[1]
+protocol=sys.argv[2]
+
+if protocol=="IP":
+    socket_protocol=0x0800
+    print "Caputre IP packets"
+elif protocol=="ARP":
+    socket_protocol=0x0806
+    print "Capture ARP packets"
+
+sock=socket.socket(socket.PF_PACKET,socket.SOCK_RAW,socket.htons(socket_protocol))
+msg=sock.recvfrom(128)
+if msg!=None:
+    msg_len=len(msg[0])
+    print "msg length is %d, and details are:" % msg_len
+    lines=msg_len//8+1
+    for i in range(lines):
+        for j in range(8):
+            index=i*8+j
+            if index>=msg_len:
+                break;
+            char=ord(msg[0][index])
+            if char&0xf0==0:
+                hex_str="0%0x\t" % char
+            else:
+                hex_str="%0x\t" % char
+            print hex_str,
+        print ''
+
