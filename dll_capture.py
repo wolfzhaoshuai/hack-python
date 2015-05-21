5a01eaa0af22233dc7e3547aab5adaf87add112d
wolfzhaoshuai <15165268607@163.com>
2015-05-21 09:28:31 +0800

    optimize the overall structure

--- dll_capture.py
+++ dll_capture.py
@@ -8,23 +8,9 @@ import sys
     Ehternet layer capture
 '''
 
-if len(sys.argv)!=3:
-    print "Usage:./dll_capture Host [IP|ARP]"
-    sys.exit(1)
-
-host=sys.argv[1]
-protocol=sys.argv[2]
-
-if protocol=="IP":
-    socket_protocol=0x0800
-    print "Caputre IP packets"
-elif protocol=="ARP":
-    socket_protocol=0x0806
-    print "Capture ARP packets"
-
-sock=socket.socket(socket.PF_PACKET,socket.SOCK_RAW,socket.htons(socket_protocol))
-msg=sock.recvfrom(128)
-if msg!=None:
+def show_details(msg):
+    '''show the details in hexdecimal type of one packet'''
+    
     msg_len=len(msg[0])
     print "msg length is %d, and details are:" % msg_len
     lines=msg_len//8+1
@@ -40,4 +26,28 @@ if msg!=None:
                 hex_str="%0x\t" % char
             print hex_str,
         print ''
+
+
+def main():
+    if len(sys.argv)!=3:
+        print "Usage:./dll_capture Host [IP|ARP]"
+        sys.exit(1)
+
+    host=sys.argv[1]
+    protocol=sys.argv[2]
+
+    if protocol=="IP":
+        socket_protocol=0x0800
+        print "Caputre IP packets"
+    elif protocol=="ARP":
+        socket_protocol=0x0806
+        print "Capture ARP packets"
+
+    sock=socket.socket(socket.PF_PACKET,socket.SOCK_RAW,socket.htons(socket_protocol))
+    msg=sock.recvfrom(128)
+    if msg!=None:
+        show_details(msg)
+
+if __name__=="__main__":
+    main()
 
